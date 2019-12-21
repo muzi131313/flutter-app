5b97cdbf1a6046dcda1fead5fbb2f57dcf95d5a5
roastwind <roastwind@163.com>
2019-12-21 22:51:14 +0800

    feat: Building Beautiful UIs with Flutter 1. create a chat ui with animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,186 +1,251 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
-void main() => runApp(MyApp());
+// doc: https://codelabs.developers.google.com/codelabs/flutter/index.html#8
+// github:  git clone https://github.com/flutter/friendlychat-steps.git
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+void main() {
+  runApp(
+    new FriendlychatApp(),
+    // new MaterialApp(
+    //   title: "Friendlychat",
+    //   home: new Scaffold(
+    //     appBar: new AppBar(
+    //       title: new Text("Friendlychat"),
+    //     ),
+    //   ),
+    // ),
+  );
+}
+
+class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      routes: {
-        "/": (context) => MyHomePage(title: 'First APP'),
-        "new_page": (context) {
-          return NewRoute(
-            text: ModalRoute.of(context).settings.arguments,
-          );
-        },
-      },
-      initialRoute: "/",
+    return new MaterialApp(
+      title: "Friendlychat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS //new
+          ? kIOSTheme //new
+          : kDefaultTheme,
+      home: new ChatScreen(),
     );
   }
 }
 
-class NewRoute extends StatelessWidget {
-  // 接收参数
-  NewRoute({
-    Key key,
-    @required this.text,
-  }) : super(key: key);
+class ChatScreen extends StatefulWidget {
+  @override //new
+  State createState() => new ChatScreenState();
 
-  final String text;
-
-  // 生成随机字符串
-  final wordPair = new WordPair.random();
-
-  @override
+  // @override
   Widget build(BuildContext context) {
-    // 获取路由参数
-    // var text = ModalRoute.of(context).settings.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("New route"),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(18),
-        child: Center(
-          child: Column(children: <Widget>[
-            Text("This is new route"),
-            Text(text),
-            Text(wordPair.toString()),
-            RaisedButton(
-              // 回传给上一个页面参数
-              onPressed: () => Navigator.pop(context, "我是返回值"),
-              child: Text("返回"),
-            )
-          ]),
-        ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Friendlychat"),
+        elevation:
+            Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0, //new
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _messages = <ChatMessage>[];
+  final TextEditingController _textController =
+      new TextEditingController(); //new
+  bool _isComposing = false;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+  Widget _buildTextComposer() {
+    return new IconTheme(
+      //new
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          //new
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            //new
+            new Flexible(
+              //new
+              child: new TextField(
+                controller: _textController,
+                onChanged: (String text) {
+                  //new
+                  setState(() {
+                    //new
+                    _isComposing = text.length > 0; //new
+                  }); //new
+                },
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    new InputDecoration.collapsed(hintText: "Send a message"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new Container(
+              //new
+              margin: new EdgeInsets.symmetric(horizontal: 4.0), //new
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? new CupertinoButton(
+                      //new
+                      child: new Text("Send"), //new
+                      onPressed: _isComposing //new
+                          ? () => _handleSubmitted(_textController.text) //new
+                          : null,
+                    )
+                  : new IconButton(
+                      //new
+                      icon: new Icon(Icons.send), //new
+                      // onPressed: () => _handleSubmitted(_textController.text),
+                      onPressed: _isComposing
+                          ? () =>
+                              _handleSubmitted(_textController.text) //modified
+                          : null,
+                    ), //new
             ),
-            FlatButton(
-              // child: Text("open new route"),
-              textColor: Colors.blue,
-              onPressed: () async {
-                //导航到新路由
-                // var result = await Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) {
-                //       return NewRoute(
-                //         // 传入路由参数
-                //         text: "我是新对象",
-                //       );
-                //     },
-                //   ),
-                // );
-                // 使用路由名称加载新路由
-                var result = await Navigator.pushNamed(
-                  context,
-                  "new_page",
-                  arguments: "我是新对象",
-                );
-                // 拿到路由返回的值
-                print("路由的值: $result");
-              },
-              child: Text("打开提示页"),
+          ], //new
+        ),
+        // child: new TextField(
+        //   controller: _textController,
+        //   onSubmitted: _handleSubmitted,
+        //   decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+        // ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    _textController.clear();
+    setState(() {
+      //new
+      _isComposing = false; //new
+    });
+    ChatMessage message = new ChatMessage(
+      text: text,
+      animationController: new AnimationController(
+        //new
+        duration: new Duration(milliseconds: 700), //new
+        vsync: this, //new
+      ),
+    ); //new
+    setState(() {
+      //new
+      _messages.insert(0, message); //new
+    });
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    //new
+    for (ChatMessage message in _messages) //new
+      message.animationController.dispose(); //new
+    super.dispose(); //new
+  }
+
+  @override //new
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            //new
+            new Flexible(
+              //new
+              child: new ListView.builder(
+                //new
+                padding: new EdgeInsets.all(8.0), //new
+                reverse: true, //new
+                itemBuilder: (_, int index) => _messages[index], //new
+                itemCount: _messages.length, //new
+              ), //new
+            ), //new
+            new Divider(height: 1.0), //new
+            new Container(
+              //new
+              decoration:
+                  new BoxDecoration(color: Theme.of(context).cardColor), //new
+              child: _buildTextComposer(), //modified
+            ), //new
+          ], //new
+        ),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS //new
+            ? new BoxDecoration(
+                //new
+                border: new Border(
+                  //new
+                  top: new BorderSide(color: Colors.grey[200]), //new
+                ), //new
+              ) //new
+            : null,
+      ),
+    );
+  }
+}
+
+const String _name = "Your Name";
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text, this.animationController});
+  final String text;
+  final AnimationController animationController;
+
+  @override
+  Widget build(BuildContext context) {
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+        //new
+        parent: animationController,
+        curve: Curves.easeOut,
+      ), //new
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
             ),
+            new Expanded(
+              //new
+              child: new Column(
+                //modified
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(_name, style: Theme.of(context).textTheme.subhead),
+                  new Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: new Text(text),
+                  ),
+                ],
+              ),
+            ),
+            // new Column(
+            //   crossAxisAlignment: CrossAxisAlignment.start,
+            //   children: <Widget>[
+            //     new Text(_name, style: Theme.of(context).textTheme.subhead),
+            //     new Container(
+            //       margin: const EdgeInsets.only(top: 5.0),
+            //       child: new Text(text),
+            //     ),
+            //   ],
+            // ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
